68d9188d85ca26f19d4900842a9f275fcc049a8e
sierra <sierra@usernoMacBook-Pro.local>
2018-06-03 20:24:18 +0900

    いろいろ変更

--- tamarb.rb
+++ tamarb.rb
@@ -1,35 +1,11 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
 class Tamarb < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/Tama-rb/homebrew-logo/tree/master/release/tamarb.tar.gz"
+  desc "Tamarb Logo"
+  homepage "https://github.com/Tama-rb/homebrew-logo"
+  url "https://github.com/Tama-rb/homebrew-logo.git"
   #sha256 "f2d30734c30eefc3a192a5395abcbc572c5cdee7d53aa80e34a6eb873d56e65f"
-  version "0.1"
-  # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test tamarb`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    bin.install "tamarb"
   end
 end
